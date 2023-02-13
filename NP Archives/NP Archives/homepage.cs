d05fb71466c774eabc10902569f7ffcbc520eb8a
technotoxs07 <technotoxs07@gmail.com>
2023-02-13 21:38:08 +0545

    update commit

--- NP Archives/NP Archives/homepage.cs
+++ NP Archives/NP Archives/homepage.cs
@@ -168,7 +168,7 @@ namespace NP_Archives
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Only For Bachelor's And Authenticated User", MessageBoxButtons.RetryCancel.ToString());
+            MessageBox.Show("Only For CSIT And Admin", MessageBoxButtons.RetryCancel.ToString());
         }
     }
 }
